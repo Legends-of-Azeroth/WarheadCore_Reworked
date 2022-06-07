ed4b09fc49c892b5268acbee44179355087936a3
devovh <devovh@gmail.com>
2022-06-07 16:06:32 +0200

    * Add Arena Spectator NPC.

---
+++ modules/mod-npc-spectator/src/ArenaSpectatorNPC.h
@@ -0,0 +1,40 @@
+#ifndef _ArenaSpectatorNPC_H_
+#define _ArenaSpectatorNPC_H_
+
+#include "BattlegroundMgr.h"
+#include "Common.h"
+#include "Player.h"
+
+enum NpcSpectatorActions {
+    NPC_SPECTATOR_ACTION_2V2_GAMES = 1000,
+    NPC_SPECTATOR_ACTION_3V3_GAMES = 2000,
+    NPC_SPECTATOR_ACTION_5V5_GAMES = 3000,
+    NPC_SPECTATOR_ACTION_SPECIFIC = 500,
+    NPC_SPECTATOR_ACTION_SELECTED_PLAYER = 4000
+};
+
+class ArenaSpectatorNPC
+{
+public:
+    static ArenaSpectatorNPC* instance();
+
+    std::string GetClassIconById(uint8 id);
+    std::string GetGamesStringData(Battleground* team, uint16 mmr, uint16 mmrTwo, std::string firstTeamName, std::string secondTeamName);
+    ObjectGuid GetFirstPlayerGuid(Battleground* team);
+    std::string GetMatchCount(uint8 type);
+    void GetMatchInformation(Battleground* arena, Player* target, uint32& firstTeamId, std::string& firstTeamName, std::string& secondTeamName, uint16& mmr, uint16& mmrTwo);
+    void ShowPage(Player* player, uint16 page, uint32 IsTop);
+    void AddBGToMap(Battleground* bg);
+    void RemoveBGFromMap(Battleground* bg);
+    void ClearBGMap();
+
+private:
+    const uint8 GamesOnPage = 14;
+
+    typedef std::unordered_map<uint32, Battleground*> BGMap;
+    BGMap _bgMap;
+};
+
+#define sArenaSpectatorNPC ArenaSpectatorNPC::instance()
+
+#endif
